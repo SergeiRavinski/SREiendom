61b132d543b8c8d97bc8a869c141e3d520351949
Sergei_ravinski <Ravinski.sergei@gmail.com>
2023-05-05 17:32:12 +0200

    Feature/animation on scroll (#45)
    * Imported AnimationOnScroll js
    
    * Created function to animate on scroll

---
+++ _app/js/modules/animation_on_scroll.js
@@ -0,0 +1,14 @@
+export default function AnimationOnScroll() {
+	const filteringHeader = document.querySelector('.filter-buttons');
+	const offset = 50;
+	
+	window.addEventListener('scroll', animateElements);
+
+	function animateElements() {
+		if (window.pageYOffset >= offset) {
+			filteringHeader.classList.add('filter-buttons__container--animated');
+		} else {
+			filteringHeader.classList.remove('filter-buttons__container--animated');
+		}	
+	}
+}

8fb496224d4c6841d17243073017ff7b722202d4
sergei <ravinski.sergei@gmail.com>
2023-05-05 17:30:31 +0200

    Imported AnimationOnScroll js

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,13 +1,15 @@
 import Header from "./modules/header.js"
-import FrontpageAccommodations from "./modules/frontpage_accommodations.js"
+import AnimationOnScroll from "./modules/animation_on_scroll.js"
 import HeaderFiltering from "./modules/header_filtering.js"
+import FrontpageAccommodations from "./modules/frontpage_accommodations.js"
 import Search from "./modules/search.js"
 import AsideCollapsibleButtons from "./modules/aside_collapsible_buttons.js"
 import ScrollUp from "./modules/scroll_up.js"
 
 Header();
-FrontpageAccommodations();
+AnimationOnScroll();
 HeaderFiltering();
+FrontpageAccommodations();
 Search();
 AsideCollapsibleButtons();
 ScrollUp();

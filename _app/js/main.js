24ca7dbd0ad84faa3fe284cb9a5f86cb59dd084f
Sergei_ravinski <Ravinski.sergei@gmail.com>
2023-05-05 16:50:28 +0200

    Feature/header filter (#36)
    * Imported icon
    
    * Imported header filtering
    
    * Changed names for variables
    
    * Created function to filter accommodations

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,5 +1,7 @@
 import Header from "./modules/header.js"
 import FrontpageAccommodations from "./modules/frontpage_accommodations.js"
+import HeaderFiltering from "./modules/header_filtering.js"
 
 Header();
 FrontpageAccommodations();
+HeaderFiltering();

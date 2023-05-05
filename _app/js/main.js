ce24b84d8f365547c8410dd969611bfc025d2541
sergei <ravinski.sergei@gmail.com>
2023-05-05 17:23:23 +0200

    Imported AsideCollapsibleButtons js

--- _app/js/main.js
+++ _app/js/main.js
@@ -2,10 +2,12 @@ import Header from "./modules/header.js"
 import FrontpageAccommodations from "./modules/frontpage_accommodations.js"
 import HeaderFiltering from "./modules/header_filtering.js"
 import Search from "./modules/search.js"
+import AsideCollapsibleButtons from "./modules/aside_collapsible_buttons.js"
 import ScrollUp from "./modules/scroll_up.js"
 
 Header();
 FrontpageAccommodations();
 HeaderFiltering();
 Search();
+AsideCollapsibleButtons();
 ScrollUp();

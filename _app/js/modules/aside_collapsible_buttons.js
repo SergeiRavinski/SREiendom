18ace83d1bfeeb132cc0b8afa7bee35ef16bb1be
Sergei_ravinski <Ravinski.sergei@gmail.com>
2023-05-07 22:26:12 +0200

    Cleaned up (#48)

--- _app/js/modules/aside_collapsible_buttons.js
+++ _app/js/modules/aside_collapsible_buttons.js
@@ -1,18 +1,11 @@
 export default function asideCollapsibleButtons() {
-	//let isCollapsed = true;
-
 	const filterButtons = document.querySelectorAll('.filtering__collapsible-button');
 
 	for (const button of filterButtons) {
 		button.addEventListener('click', handleClickFilterButton);
 	}
-	
-	//function filterSectionVisibility() {
-	//	isCollapsed = !isCollapsed;
-	//}
 
 	function handleClickFilterButton(event) {
-		//filterSectionVisibility();
 		renderHTML(event);
 	}
 
@@ -22,11 +15,5 @@ export default function asideCollapsibleButtons() {
 		
 		currentOptions.style.display = currentOptions.style.display === 'block' ? 'none' : 'block';
 		currentButtonArrow.style.transform = currentButtonArrow.style.transform === 'rotateX(180deg)' ? 'rotateX(0deg)' : 'rotateX(180deg)';
-
-		//if (isCollapsed === false) {
-		//	currentOptions.classList.add('filtering__property-type-options--visible');
-		//} else {
-		//	currentOptions.classList.remove('filtering__property-type-options--visible');
-		//}
 	}
 }

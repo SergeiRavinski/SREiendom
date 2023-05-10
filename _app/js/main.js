83c36997820f7dc9946b4948020422af4e0eff70
sergei <ravinski.sergei@gmail.com>
2023-05-10 13:21:21 +0200

    Imported aside filtering

--- _app/js/main.js
+++ _app/js/main.js
@@ -4,6 +4,7 @@ import HeaderFiltering from "./modules/header_filtering.js"
 import FrontpageAccommodations from "./modules/frontpage_accommodations.js"
 import Search from "./modules/search.js"
 import AsideCollapsibleButtons from "./modules/aside_collapsible_buttons.js"
+import AsideFiltering from "./modules/aside_filtering.js"
 import ScrollUp from "./modules/scroll_up.js"
 import ScrollIntoSection from "./modules/scroll_into_section.js"
 
@@ -13,5 +14,6 @@ HeaderFiltering();
 FrontpageAccommodations();
 Search();
 AsideCollapsibleButtons();
+AsideFiltering();
 ScrollUp();
 ScrollIntoSection();

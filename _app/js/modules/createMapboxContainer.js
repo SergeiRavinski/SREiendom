4549d500f6addade13b0e5acc17267f1b5846ea7
sergei <ravinski.sergei@gmail.com>
2023-05-15 09:29:13 +0200

    Created mapbox

---
+++ _app/js/modules/createMapboxContainer.js
@@ -0,0 +1,44 @@
+import { t_map } from '../env.js';
+
+export default function createMapboxContainerDOM(accommodations) {
+	mapboxgl.accessToken = t_map;
+	let center = [10.757933, 59.911491];
+	const bounds = new mapboxgl.LngLatBounds();
+	const zoomLevel = 5
+	
+	const map = new mapboxgl.Map({
+		container: 'filtering__mapbox',
+		style: 'mapbox://styles/mapbox/streets-v12',
+		center: center,
+		zoom: zoomLevel,
+		cooperativeGestures: true
+	});
+	
+	accommodations.forEach(accommodation => {
+		const markerElement = document.createElement('div');
+		const coordinates = [`${accommodation.longitude.longitude}`, `${accommodation.latitude.latitude}`];
+
+		markerElement.classList.add('marker');
+		const popup = new mapboxgl
+			.Popup({ offset: 27 })
+			.setHTML('<a href="' + '#' + '">' + accommodation.title + '</a>')
+
+		new mapboxgl.Marker(markerElement)
+			.setLngLat(coordinates) 
+			.setPopup(popup)
+			.addTo(map)
+		
+		markerElement.addEventListener('click', () => {
+			map.flyTo({
+				center: ([`${accommodation.longitude.longitude}`, `${accommodation.latitude.latitude}`]),
+				essential: true,
+				zoom: 10  
+			});
+		});
+		bounds.extend(coordinates);
+	});
+	
+	map.addControl(new mapboxgl.FullscreenControl());
+	map.addControl(new mapboxgl.NavigationControl());
+	map.fitBounds(bounds, { padding: 30 });
+}

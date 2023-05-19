e0b89261cba51bb6148de141d5c44e605af7c0d9
sergei <ravinski.sergei@gmail.com>
2023-05-19 13:22:19 +0200

    Removed popup

--- _app/js/modules/popup.js
+++
@@ -1,7 +0,0 @@
-import { sanity } from '../sanity.js';
-//import createMapboxContainerDOM from './create_mapbox_container.js';
-//import createAccommodationContainerDOM from "./accommodation_container.js";
-
-export default async function Popup() {
-	
-}

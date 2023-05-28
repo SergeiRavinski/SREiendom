391a670cb972acc632f7fb45cf8ff065c22dc062
sergei <ravinski.sergei@gmail.com>
2023-05-28 14:25:37 +0200

    Added price filtering

--- _app/js/modules/aside_filtering.js
+++ _app/js/modules/aside_filtering.js
@@ -1,7 +1,7 @@
 import { sanity } from '../sanity.js';
 import createAccommodationContainerDOM from "../util/accommodation_container.js";
 import createMapboxContainerDOM from '../util/create_mapbox_container.js';
-import FrontpageAccommodations from './frontpage_accommodations.js';
+//import FrontpageAccommodations from './frontpage_accommodations.js';
 import createPopup from './create_popup.js';
 import slideshow from '../util/slideshow.js';
 import wishlist from './wishlist.js';
@@ -14,12 +14,20 @@ export default async function AsideFiltering() {
 	let countyParameter = '';
 	let bedsParameter = '';
 	let accommodations = [];
+	let minPrice = '';
+	let maxPrice = '';
+	let minPriceParameter = '';
+	let maxPriceParameter = '';
 
 	const groqQuery = `_type == 'accommodation' `;
 	const asideButtons = document.querySelectorAll('.filtering__collapsible-button');
 	const sectionFronpageAccommodations = document.querySelector('.frontpage_accommodations');
 	const filterButtons = document.querySelectorAll('.filter-buttons__container button');
 	const filterButtonAllAccomodation = document.querySelector('.filter-buttons__allaccommodations');
+	const priceForm = document.querySelector('.filtering__price-range');
+	const minPriceInput = document.querySelector('.filtering__price-range-min-price');
+	const maxPriceInput = document.querySelector('.filtering__price-range-max-price');
+	const messageTitle = document.querySelector('.frontpage_accommodations__message');
 
 	for (const asideButton of asideButtons) {
 		asideButton.addEventListener('click', handleAsideButtonClick);
@@ -34,8 +42,23 @@ export default async function AsideFiltering() {
 		}
 	}
 
+	priceForm.addEventListener('submit', handlePriceFilteringButtonClick);
+
 	async function handleCheckboxClick(event) {
 		getCheckboxDataFilter(event);
+		addPropertyTypeParameter();
+		addCountyParameter();
+		addBedsParameter();
+		await fetchAccommodations();
+		renderHTML();
+		createMapboxContainerDOM(accommodations);
+		wishlist();
+	}
+
+	async function handlePriceFilteringButtonClick(event) {
+		event.preventDefault();
+		addMinPriceFiltering();
+		addMaxPriceFiltering();
 		await fetchAccommodations();
 		renderHTML();
 		createMapboxContainerDOM(accommodations);
@@ -73,8 +96,8 @@ export default async function AsideFiltering() {
 	}
 
 	function getCheckboxDataFilter(event) {
-		let currentDataFilter = event.currentTarget.dataset.filter;
-		let currentCheckbox = event.currentTarget;
+		const currentDataFilter = event.currentTarget.dataset.filter;
+		const currentCheckbox = event.currentTarget;
 
 		if (currentCheckbox.className === 'filtering__checkbox filtering__checkbox-propertytype') {
 			addAndRemoveDataFilterPropertyType(currentDataFilter, currentCheckbox);
@@ -88,9 +111,9 @@ export default async function AsideFiltering() {
 			addAndRemoveDataFilterBeds(currentDataFilter, currentCheckbox);	
 		}
 
-		if (checkboxDataFilterPropertyType.length === 0 && checkboxDataFilterCounty.length === 0 && checkboxDataFilterBeds.length === 0) {
-			FrontpageAccommodations();
-		}
+		//if (checkboxDataFilterPropertyType.length === 0 && checkboxDataFilterCounty.length === 0 && checkboxDataFilterBeds.length === 0) {
+		//	FrontpageAccommodations();
+		//}
 	}
 
 	function addPropertyTypeParameter() {
@@ -117,12 +140,28 @@ export default async function AsideFiltering() {
 		}
 	}
 
-	async function fetchAccommodations() {
-		addPropertyTypeParameter();
-		addCountyParameter();
-		addBedsParameter();
+	function addMinPriceFiltering() {
+		minPrice = minPriceInput.value;
 
-		let query = `*[${groqQuery}${propertyParameter}${countyParameter}${bedsParameter}] | order(price asc) {
+		if (minPrice !== '') {
+			minPriceParameter = ` && price >= $lowestPrice`;
+		} else {
+			minPriceParameter = '';
+		}
+	}
+
+	function addMaxPriceFiltering() {
+		maxPrice = maxPriceInput.value;
+
+		if (maxPrice !== '') {
+			maxPriceParameter = ` && price <= $highestPrice`;
+		} else {
+			maxPriceParameter = '';
+		}
+	}
+
+	async function fetchAccommodations() {
+		let query = `*[${groqQuery}${propertyParameter}${countyParameter}${bedsParameter}${minPriceParameter}${maxPriceParameter}] | order(price asc) {
 			"image": gallery[0].asset->url,
 			"images": gallery[].asset->url,
 			"county": county->name,
@@ -140,13 +179,21 @@ export default async function AsideFiltering() {
 		const params = {
 			propertyType: checkboxDataFilterPropertyType,
 			countyName: checkboxDataFilterCounty,
-			bedQuantity: checkboxDataFilterBeds
+			bedQuantity: checkboxDataFilterBeds,
+			lowestPrice: Number(minPrice),
+			highestPrice: Number(maxPrice)
 		};
-		
+
 		accommodations = await sanity.fetch(query, params);
 	}
 
 	function renderHTML() {
+		if (accommodations.length === 0) {
+			messageTitle.classList.add('frontpage_accommodations__message--visible');
+		} else {
+			messageTitle.classList.remove('frontpage_accommodations__message--visible');
+		}
+
 		for (const button of filterButtons) {
 			button.classList.remove('filter-buttons--active');
 		}
@@ -154,6 +201,10 @@ export default async function AsideFiltering() {
 		filterButtonAllAccomodation.classList.add('filter-buttons--active');
 	
 		sectionFronpageAccommodations.innerHTML = '';
+		minPriceInput.value = '';
+		maxPriceInput.value = '';
+		minPriceParameter = '';
+		maxPriceParameter = '';
 
 		accommodations.forEach(element => {
 			let accommodationTemplate = createAccommodationContainerDOM(element);
